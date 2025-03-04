16c9ddd02d2c1dea07ef248df378a610a9f06771
aferferc <aferferc@g.educaand.es>
2025-03-04 17:42:31 +0100

    Update eventos.js

--- eventos.js/eventos.js
+++ eventos.js/eventos.js
@@ -1,20 +1,46 @@
 document.getElementsByClassName("form-ini").addEventListener("submit", function(event){
-    let usuario = String(document.getElementById("usu-ini-in").value);
-    let email = String(document.getElementById("email-ini-in").value);
-    let contrasena = String(document.getElementById("cont-ini-in").value);
+    let usuarioIni = String(document.getElementById("usu-ini-in").value);
+    let emailIni = String(document.getElementById("email-ini-in").value);
+    let contrasenaIni = String(document.getElementById("cont-ini-in").value);
 
-    if(usuario === "" || usuario.length() > 10){
-        alert("No as introducido el usuario o este es mayor de 8 caracteres")
+    if(usuarioIni === "" || usuarioIni.length() > 10){
+        alert("No has introducido el usuario o este es mayor de 8 caracteres");
         event.preventDefault();
     }
-    else if(email ==="" || email.includes("@gmail.com") == false){
-        alert("No as introducido el email o este no acaba en @gmail.com")
+    else if(emailIni === "" || emailIni.includes("@gmail.com") == false){
+        alert("No has introducido el email o este no acaba en @gmail.com");
         event.preventDefault();
     }
-    else if(contrasena ==="" || contrasena.length() < 8){
-        alert("No as introducido el email o este no acaba en @gmail.com")
+    else if(contrasenaIni === "" || contrasenaIni.length() < 8){
+        alert("No has introducido la contraseña o esta tiene menos de 8 caracteres");
         event.preventDefault();
     }
+});
 
+document.getElementsByClassName("form-reg").addEventListener("submit", function(event){
+    let usuarioReg = String(document.getElementById("usu-ini-in").value);
+    let emailReg = String(document.getElementById("email-ini-in").value);
+    let contrasenaReg = String(document.getElementById("cont-ini-in").value);
+    let contrasenaConReg = String(document.getElementById("conf-cont-reg-in").value);
 
+    if(usuarioReg === "" || usuarioReg.length() > 10){
+        alert("No has introducido el usuario o este es mayor de 8 caracteres");
+        event.preventDefault();
+    }
+    else if(emailReg === "" || emailReg.includes("@gmail.com") == false){
+        alert("No has introducido el email o este no acaba en @gmail.com");
+        event.preventDefault();
+    }
+    else if(contrasenaReg === "" || contrasenaReg.length() < 8){
+        alert("No has introducido la contraseña o esta tiene menos de 8 caracteres");
+        event.preventDefault();
+    }
+    else if(contrasenaConReg === "" || contrasenaConReg.length() < 8){
+        alert("No has introducido la contraseña de confirmacion o esta tiene menos de 8 caracteres");
+        event.preventDefault();
+    }
+    else if(contrasenaReg != contrasenaConReg){
+        alert("La contraseña y la contraseña de confirmacion no coinciden");
+        event.preventDefault();
+    }
 });
